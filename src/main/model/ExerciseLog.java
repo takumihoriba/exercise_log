501df11a1bc902102daa332ce7d43173baffffb3
tonyh6369 <pioneering.sea@gmail.com>
2022-10-10 18:25:20 -0700

    Implement ExerciseLog and its UI

---
+++ src/main/model/ExerciseLog.java
@@ -0,0 +1,103 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseLog {
+    private List<Integer> time;
+    private List<String> activity;
+    private List<String> sports;
+    private List<Integer> timeByActivity;
+    private int goal;
+
+    // REQUIRES: none
+    // EFFECTS: instantiates 3 lists and set default value for goal and sports
+    public ExerciseLog() {
+        time = new ArrayList<>();
+        activity = new ArrayList<>();
+        sports = new ArrayList<>();
+        timeByActivity = new ArrayList<>();
+        sports.add("running");
+        sports.add("swimming");
+        sports.add("cycling");
+        goal = 1000;
+    }
+
+    // REQUIRES: String activity must be an element of sports.
+    // MODIFIES: this
+    // EFFECTS: add time and name of activity to the lists.
+    public void logExercise(int time, String activity) {
+
+        this.time.add(time);
+        this.activity.add(activity);
+    }
+
+    //REQUIRES: none.
+    //MODIFIES: none.
+    //EFFECTS: get the sport that the user did the least.
+    public String recommend() {
+        calculateTimeByActivity();
+        int min = this.goal;
+        int minIdx = 0;
+        int size = timeByActivity.size();
+        for (int i = 0; i < size; i++) {
+            if (timeByActivity.get(i) < min) {
+                min = timeByActivity.get(i);
+                minIdx = i;
+            }
+        }
+        String sport = sports.get(minIdx);
+        return sport;
+    }
+
+    //REQUIRES: none.
+    //MODIFIES: this
+    //EFFECTS: modifies timeByActivity by finding the sum of minutes for each exercise
+    public void calculateTimeByActivity() {
+        timeByActivity = new ArrayList<>();
+        for (String s: sports) {
+            timeByActivity.add(0);
+        }
+        for (int i = 0; i < time.size(); i++) {
+            String activityAtIth = activity.get(i);
+            for (int j = 0; j < sports.size(); j++) {
+                String sportAtJth = sports.get(j);
+                if (activityAtIth.equals(sportAtJth)) {
+                    int updatedTime = timeByActivity.get(j) + time.get(i);
+                    timeByActivity.set(j, updatedTime);
+                }
+            }
+        }
+    }
+
+    //REQUIRES: none.
+    //MODIFIES: none.
+    //EFFECTS: return integer representing the difference
+    // between sum of all minutes and goal
+    public int distanceToGoal() {
+        int sum = 0;
+        for (Integer i: this.time) {
+            sum += i;
+        }
+        return goal - sum;
+    }
+
+    //REQUIRES: new goal must be positive
+    //MODIFIES: this
+    //EFFECTS: change goal to a new quantity
+    public void modifyGoal(int newGoal) {
+        this.goal = newGoal;
+    }
+
+
+
+    public List<String> getSports() {
+        return sports;
+    }
+
+    public int getGoal() {
+        return this.goal;
+    }
+
+
+}
---
+++ src/main/ui/ExerciseTracker.java
@@ -0,0 +1,94 @@
+package ui;
+
+import model.ExerciseLog;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExerciseTracker {
+    private ExerciseLog log;
+    private Scanner input;
+
+    public ExerciseTracker() {
+        runExerciseLog();
+    }
+
+    public void runExerciseLog() {
+        boolean running = true;
+        char userChoice = '0';
+        setup();
+        progress();
+
+        while (running) {
+            showMenu();
+            userChoice = input.nextLine().toLowerCase().charAt(0);
+
+            if (userChoice == 'e') {
+                running = false;
+                System.out.println("See you!");
+            } else {
+                switch (userChoice) {
+                    case 'a': record();
+                    break;
+                    case 'r': getRec();
+                    break;
+                    case 'p': progress();
+                    break;
+                    case 'c': changeGoal();
+                    break;
+                    default: System.out.println("Your choice is invalid");
+                }
+            }
+
+        }
+    }
+
+    private void changeGoal() {
+        System.out.print("Enter your new goal (in minutes): ");
+        int newGoal = input.nextInt();
+        input.nextLine();
+        log.modifyGoal(newGoal);
+    }
+
+
+    private void getRec() {
+        System.out.println("thinking based on your record....");
+        System.out.println("How about " + log.recommend() + " ?");
+    }
+
+    private void record() {
+        System.out.println("what did you do? Pick one from the following: ");
+        System.out.println(log.getSports().toString());
+        String activity = input.nextLine();
+        System.out.print("How many minutes did you do it?: ");
+        int time = input.nextInt();
+        input.nextLine();
+        log.logExercise(time, activity);
+        System.out.println("Your record has been saved successfully");
+    }
+
+    private void showMenu() {
+        System.out.println("-----------------------");
+        System.out.println("a -> add a new log to your record");
+        System.out.println("r -> get a recommendation");
+        System.out.println("p -> show progress");
+        System.out.println("e -> exit this application");
+    }
+
+    public void setup() {
+        log = new ExerciseLog();
+        input = new Scanner(System.in);
+    }
+
+    public void progress() {
+        System.out.println("Your current goal: " + log.getGoal());
+        if (log.distanceToGoal() <= 0) {
+            System.out.println("You reached your goal. Congrats!");
+        } else {
+            System.out.println(log.distanceToGoal() + " min to your goal.");
+            System.out.println("Available sports:" + log.getSports().toString());
+        }
+
+    }
+}
